0f9f2696436d59f3ef9ee57f6cbb000270e1dc7b
Sushma Koodali <sushma.gargya@outlook.com>
2026-07-21 21:52:33 +0200

    Task 1 Displaying cards completed

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,19 @@
 import "./Color.css";
 
 export default function Color({ color }) {
-  return null;
+  return (
+    <article>
+      <div
+        className="color-card"
+        style={{ background: color.hexValue, color: color.contrastText }}
+      >
+        <h3 className="color-headline">Role: {color.role}</h3>
+        hex-Value:<span className="colorcard__hexValue">{color.hexValue}</span>
+        <br />
+        Contrast-Text:
+        <span className="colorcard__contrastText">{color.contrastText}</span>
+      </div>
+      <br />
+    </article>
+  );
 }
