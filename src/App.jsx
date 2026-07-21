b156f95e08671443737571ab0e8f64c1dd5c6a53
Sushma Koodali <sushma.gargya@outlook.com>
2026-07-21 23:44:45 +0200

    UI development started

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,38 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Theme Creator</h1>
-      <ul className="color-List">
-        {initialColors.map((color) => (
-          <li key={color.id}>
-            <Color color={color} />
-          </li>
-        ))}
-      </ul>
+      <div className="split-container">
+        {/* Left Independent Panel */}
+        <section className="panel panel-left">
+          <h1>Theme List</h1>
+          <ul className="color-List">
+            {initialColors.map((color) => (
+              <li key={color.id}>
+                <Color color={color} />
+              </li>
+            ))}
+          </ul>
+        </section>
+        {/* Right Independent Panel */}
+        <section className="panel panel-right">
+          <h1>Theme Creator</h1>
+          <form className="theme-creator-form">
+            <fieldset>
+              <legend>Create your own theme</legend>
+              <label htmlFor="role">Role:</label>
+              <input type="text" id="role" name="role" />
+              <br />
+              <label htmlFor="hexValue">Hex Value:</label>
+              <input type="color" id="hexValue" name="hexValue" />
+              <br />
+              <label htmlFor="contrastText">Contrast Text:</label>
+              <input type="color" id="contrastText" name="contrastText" />
+              <br />
+              <button type="submit">Add Theme</button>
+            </fieldset>
+          </form>
+        </section>
+      </div>
     </>
   );
 }
