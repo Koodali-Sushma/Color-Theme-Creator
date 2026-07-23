fe379b65e372955a60a790bec8a1fb63ac1b3fb0
Sushma Koodali <sushma.gargya@outlook.com>
2026-07-23 23:59:10 +0200

    Edit implementation completed

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,46 +1,16 @@
 export const initialColors = [
   {
-    id: "c1",
-    role: "primary main",
-    hexValue: "#ff4a11",
-    contrastText: "#FFFFFF",
-  }, // Vibrant orange
-  {
     id: "c2",
     role: "primary dark",
-    hexValue: "#c73e0b",
+    hexValue: "#f94f12",
     contrastText: "#FFFFFF",
   }, // Darker shade of orange
   {
-    id: "c3",
-    role: "primary light",
-    hexValue: "#ff7a3e",
-    contrastText: "#000000",
-  }, // Lighter shade of orange
-  {
     id: "c4",
     role: "secondary main",
-    hexValue: "#5C6BC0",
+    hexValue: "#2b46dce2",
     contrastText: "#FFFFFF",
   }, // Indigo Blue
-  {
-    id: "c5",
-    role: "secondary dark",
-    hexValue: "#3949AB",
-    contrastText: "#FFFFFF",
-  }, // Darker indigo blue
-  {
-    id: "c6",
-    role: "secondary light",
-    hexValue: "#9FA8DA",
-    contrastText: "#000000",
-  }, // Lighter indigo blue
-  {
-    id: "c7",
-    role: "background main",
-    hexValue: "#252629",
-    contrastText: "#FFFFFF",
-  }, // Dark charcoal
   {
     id: "c8",
     role: "background dark",
@@ -48,9 +18,41 @@ export const initialColors = [
     contrastText: "#FFFFFF",
   }, // Darker charcoal
   {
+    id: "c7",
+    role: "background main",
+    hexValue: "#92399e",
+    contrastText: "#FFFFFF",
+  }, // Violet
+  {
+    id: "c5",
+    role: "secondary dark",
+    hexValue: "#3949AB",
+    contrastText: "#d1fa6a",
+  }, // Darker  blue
+  {
+    id: "c6",
+    role: "secondary light",
+    hexValue: "#9FA8DA",
+    contrastText: "#e60909",
+  }, // Cyelon blue
+  {
+    id: "c1",
+    role: "primary main",
+    hexValue: "#fc5826de",
+    contrastText: "#FFFFFF",
+  }, // Orange
+
+  {
     id: "c9",
     role: "background light",
     hexValue: "#43464b",
     contrastText: "#FFFFFF",
-  }, // Lighter charcoal
+  }, // Grey
+
+  {
+    id: "c3",
+    role: "primary light",
+    hexValue: "#ff7a3e",
+    contrastText: "#000000",
+  }, // Lighter shade of orange
 ];
